fa6f32de4f3cce40de16e2f907a210291df9b901
dineshm20 <dineshm.20@gmail.com>
2021-10-01 21:18:44 +0530

    Today Oct1 Loops

--- Hello.java
+++ Hello.java
@@ -5,7 +5,8 @@ public class Hello {
     byte z = 1;
     byte y = (byte) x; // explicit cast
     int y1 = z; // implicit cast
-    String empname; //test for github
+    String empname; // test for github
+    String gundu = "hello";
     int empid = 1;
     int price = 100;
     static int instituecode = 1100;
@@ -20,9 +21,9 @@ public class Hello {
         System.out.println(dinesh);
     }
 
-    double getDiscount(int price) { // 0.00
-        double discount = 1;
-        discount = price * (20 / 100);
+    public static double getDiscount(int price) { // 0.00
+        double discount;
+        discount = price + 1;
         return discount; // 0.00
     }
 
@@ -36,21 +37,41 @@ public class Hello {
                                                 // classname.varname)
         // float result=h.getDiscount(h.price);
         // System.out.println(result);
-        double result2 = h.getDiscount(price);
+        double result2 = getDiscount(price);
         System.out.println(result2);
-        System.out.println("Hello World");
-        byte b = 20;
-        System.out.println("b=" + b);
-        short s = 21;
-        System.out.println("s=" + s);
+
+        // if else
+        if (h.gundu == "hello") {
+            System.out.println("Hello World");
+        } else {
+            byte b = 20;
+            System.out.println("b=" + b);
+        }
         int i = 11;
-        System.out.println("i=" + i);
-        long l = 12;
-        System.out.println("l=" + l);
+        short s = 21;
+        // if else ladder
+        if (s == 20) {
+            System.out.println("s=" + s);
+        } else if (i == 10) {
+
+            System.out.println("i=" + i);
+        } else {
+            long l = 12;
+            System.out.println("l=" + l);
+        }
         float f = 2.1f;
-        System.out.println("f=" + f);
         double d = 0.000000001;
-        System.out.println("d=" + d);
+        // nested if
+        if (f == 2.1f) {
+            if (d == 0.000000001) {
+                if (price == 999) {
+                    System.out.println("f=" + f);
+                    System.out.println("d=" + d);
+                }
+            } else {
+                System.out.println("stupid fellow ans is wrong");
+            }
+        }
         char c1 = '$';
         System.out.println("c1=" + c1);
         char c2 = '@';
@@ -59,6 +80,32 @@ public class Hello {
         System.out.println("c3=" + c3);
         boolean b1 = true;
         System.out.println("true?=" + b1);
+        // switch case
+        char n = 'b';
+        switch (n) {
+            case 'a':
+                System.out.println("zero");
+                break;
+            case 'b':
+                System.out.println("one");
+                break;
+            default:
+                System.out.println("muttal");
+        }
+
+        // for loop
+        int m, num = 1, c;
+        for (m = 1; m <= 10; m++) {
+            int result = m * num;
+            System.out.println(+num + "x" + m + "=" + result);
+        }
+        //factorialclea
+        int nu = 5, Fact=1;
+        for (i = 1; i <= nu; i++)  {
+			Fact = Fact * i;
+		}
+        System.out.println(Fact);
+
     }
 
     static {
